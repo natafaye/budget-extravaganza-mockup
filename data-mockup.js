cd2d85ea5a74fd402f0ea2e1fe54fa234ba536a0
Natalie Childs <nata.faye@gmail.com>
2021-11-21 15:05:01 -0800

    initial mockup of pages and data

---
+++ data-mockup.js
@@ -0,0 +1,33 @@
+
+const categories = [
+    {
+        _id: 0,
+        name: "Income",
+        target: 3000
+    },
+    {
+        _id: 1,
+        name: "Rent",
+        target: 1500
+    },
+    {
+        _id: 2,
+        name: "Groceries",
+        target: 400
+    }
+]
+
+const transactions = [
+    {
+        _id: 0,
+        categoryId: 0,
+        amount: 1400,
+        notes: "first paycheck"
+    },
+    {
+        _id: 1,
+        categoryId: 2,
+        amount: -45.89,
+        notes: "lots of pasta"
+    }
+]
